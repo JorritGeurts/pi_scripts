b2ec451c9fcb718cc371a979836ddd540aae6a93
JorritGeurts <r0956231@student.thomasmore.be>
2023-03-19 11:13:01 +0100

    oude Opdracht Teruggezet

--- Opdracht.py
+++ Opdracht.py
@@ -2,18 +2,19 @@ import wiringpi
 import time
 import sys
 
-def blink(_pin):
-    wiringpi.digitalWrite(_pin,1)
-    time.sleep(0.5)
-    wiringpi.digitalWrite(_pin,0)
-    time.sleep(0.5)
+# def blink(_pin):
+#     wiringpi.pinMode(i, 1)
+#     time.sleep(0.5)
 
 print("start")
-pin = 2
 wiringpi.wiringPiSetup()
-wiringpi.pinMode(pin, 1)
+Pins = [2, 3, 4, 6]
+for pi in Pins:
+    wiringpi.pinMode(pi, wiringpi.OUTPUT)
 
-for i in range(0,10):
-    blink(pin)
+for index in range(1,10):
+    for i in Pins:
+        wiringpi.pinMode(pi, wiringpi.HIGH)
+    time.sleep(1)
 
 print("done")
