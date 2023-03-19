04855f44785fe0ff0ee250c09773453ca175a057
JorritGeurts <r0956231@student.thomasmore.be>
2023-03-19 12:19:21 +0100

    Opdracht7

--- Opdracht.py
+++ Opdracht.py
@@ -1,27 +1,29 @@
-import wiringpi as wp
+import wiringpi
 import time
+import sys
 
-# set pin numbers
-LED_PIN = 2
+#def blink(_pin,_pin2):
+    #wiringpi.digitalWrite(_pin, 1)
+    #wiringpi.digitalWrite(_pin2, 1)
+    #time.sleep(0.1)
+    #wiringpi.digitalWrite(_pin, 0)
+    #wiringpi.digitalWrite(_pin2, 0)
+    #time.sleep(0.1)
 
-# initialize WiringPi
-wp.wiringPiSetup()
+#setup
+print("start")
+wiringpi.wiringPiSetup()
+Pins = [2, 3, 4, 6]
+for i in Pins:
+    wiringpi.digitalWrite(i, wiringpi.OUTPUT)
 
-# set up LED pin as output
-wp.pinMode(LED_PIN, wp.OUTPUT)
+fading = [25, 50, 75, 100]
 
-# blinking function
-def blink(pin, numberBlinks, periode, dutyCycle):
-    timeHigh = periode * dutyCycle/100
-    timeLow = periode - timeHigh
-    for i in range(numberBlinks):
-        wp.digitalWrite(pin, wp.HIGH)
-        time.sleep(timeHigh)
-        wp.digitalWrite(pin, wp.LOW)
-        time.sleep(timeLow)
+#Program
+for fade in fading:
+    for pin in Pins:
+        wiringpi.softPwmCreate(pin, 0, 100)
+        wiringpi.softPwmWrite(pin, int(level/100*100))
+    time.sleep(2)
 
-# blink LED 10 times
-blink(LED_PIN, 20, 0.5, 75)
-
-# cleanup
-print("program executed")
+print("done")
