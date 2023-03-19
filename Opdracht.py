0a8d2367efb690b2f4bcd40ae8cda31c864d5ac5
JorritGeurts <r0956231@student.thomasmore.be>
2023-03-19 11:26:02 +0100

    Opdracht2

--- Opdracht.py
+++ Opdracht.py
@@ -6,19 +6,22 @@ import sys
 #     wiringpi.pinMode(i, 1)
 #     time.sleep(0.5)
 
+#setup
 print("start")
 wiringpi.wiringPiSetup()
 Pins = [2, 3, 4, 6]
 for pi in Pins:
     wiringpi.pinMode(pi, wiringpi.OUTPUT)
 
+#lights on
 for index in range(1,10):
     for i in Pins:
         wiringpi.digitalWrite(pi, wiringpi.HIGH)
     time.sleep(1)
 
-for i in Pins:
-    wiringpi.digitalWrite(pi, wiringpi.LOW)
-time.sleep(0.5)
+#lights off
+#for i in Pins:
+#    wiringpi.digitalWrite(pi, wiringpi.LOW)
+#time.sleep(0.5)
 
 print("done")
