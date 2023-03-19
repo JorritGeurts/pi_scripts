bc7cb44e5fa4c97759e4bb1c4e75a971461edd30
JorritGeurts <r0956231@student.thomasmore.be>
2023-03-19 12:48:51 +0100

    MorseCodeMachine

--- MorseCode.py
+++ MorseCode.py
@@ -11,7 +11,135 @@ def translate(_letter):
     if _letter.upper() == "A":
         Dott()
         Dash()
+
+    if _letter.upper() == "B":
+        Dash()
+        Dott()
+        Dott()
+        Dott()
+    
+    if _letter.upper() == "C":
+        Dash()
+        Dott()
+        Dash()
+        Dott()
+
+    if _letter.upper() == "D":
+        Dash()
+        Dott()
+        Dott()
         
+    if _letter.upper() == "E":
+        Dott()
+    
+    if _letter.upper() == "G":
+        Dash()
+        Dash()
+        Dott()
+    
+    if _letter.upper() == "H":
+        Dott()
+        Dott()
+        Dott()
+        Dott()
+
+    if _letter.upper() == "I":
+        Dott()
+        Dott()
+
+    if _letter.upper() == "J":
+        Dott()
+        Dash()
+        Dash()
+        Dash()
+    
+    if _letter.upper() == "K":
+        Dash()
+        Dott()
+        Dash()
+
+    if _letter.upper() == "L":
+        Dott()
+        Dash()
+        Dott()
+        Dott()
+
+    if _letter.upper() == "M":
+        Dash()
+        Dash()
+
+    if _letter.upper() == "N":
+        Dash()
+        Dott()
+    
+    if _letter.upper() == "O":
+        Dash()
+        Dash()
+        Dash()
+
+    if _letter.upper() == "P":
+        Dott()
+        Dash()
+        Dash()
+        Dott()
+
+    if _letter.upper() == "Q":
+        Dash()
+        Dash()
+        Dott()
+        Dash()
+
+    if _letter.upper() == "R":
+        Dott()
+        Dash()
+        Dott()
+
+    if _letter.upper() == "S":
+        Dott()
+        Dott()
+        Dott()
+
+    if _letter.upper() == "T":
+        Dash()
+
+    if _letter.upper() == "U":
+        Dott()
+        Dott()
+        Dash()
+
+    if _letter.upper() == "V":
+        Dott()
+        Dott()
+        Dott()
+        Dash()
+
+    if _letter.upper() == "W":
+        Dott()
+        Dott()
+        Dash()
+    
+    if _letter.upper() == "X":
+        Dash()
+        Dott()
+        Dott()
+        Dash()
+
+    if _letter.upper() == "Y":
+        Dash()
+        Dott()
+        Dash()
+        Dash()
+
+    if _letter.upper() == "B":
+        Dash()
+        Dash()
+        Dott()
+        Dott()
+
+    if _letter.upper() == " ":
+        wiringpi.digitalWrite(pin,0)
+        time.sleep(0.1)
+
 
 def Dott():
     wiringpi.digitalWrite(pin,1)
