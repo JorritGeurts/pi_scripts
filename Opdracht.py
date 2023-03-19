df8c6222e4fb4d684b7380e2d4c2c66ada5e1004
JorritGeurts <r0956231@student.thomasmore.be>
2023-03-19 11:27:40 +0100

    Opdracht2TimeSleepFix

--- Opdracht.py
+++ Opdracht.py
@@ -17,11 +17,13 @@ for pi in Pins:
 for index in range(1,10):
     for i in Pins:
         wiringpi.digitalWrite(pi, wiringpi.HIGH)
+    time.sleep(1)
     
 
 #lights off
 for i in Pins:
     wiringpi.digitalWrite(pi, wiringpi.LOW)
+time.sleep(1) 
 
 
 print("done")
