c62fc65d23f98540113b3f075eea17954a508ec5
JorritGeurts <r0956231@student.thomasmore.be>
2023-03-19 12:41:10 +0100

    FirstTest

--- MorseCode.py
+++ MorseCode.py
@@ -7,11 +7,34 @@ wiringpi.wiringPiSetup()
 pin = 2
 wiringpi.pinMode(pin, 1)
 
+def translate(_letter):
+    if _letter.upper() == "A":
+        Dott()
+        Dash()
+        
+
+def Dott():
+    wiringpi.digitalWrite(pin,1)
+    time.sleep(0.1)
+    wiringpi.digitalWrite(pin,0)
+    time.sleep(0.1)
+
+def Dash():
+    wiringpi.digitalWrite(pin,1)
+    time.sleep(0.5)
+    wiringpi.digitalWrite(pin,0)
+    time.sleep(0.1)
+
+
+
 #message input
 message = str(input("Give your message: "))
 
 #translation
 for i in range(len(message)):
     letter = message[i]
+
+    translate(letter)
+    
 
 print(letter)
